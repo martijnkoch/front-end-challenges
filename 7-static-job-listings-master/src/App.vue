cf961fef0feec45f74383cb9e54278bef9951f1a
Martijn Koch <martijn_Waterreus@hotmail.com>
2024-05-28 11:39:32 +0200

    hover fixed

--- 7-static-job-listings-master/src/App.vue
+++ 7-static-job-listings-master/src/App.vue
@@ -7,7 +7,7 @@
   <div v-if="searchItems.length" class="container mx-auto flex items-center bg-white px-10 py-4 -mt-8 z-10 relative rounded-md gap-4 md:gap-4 flex-wrap">
     <div v-for="item in searchItems" :key="item" class="flex items-center">
       <p class="h-8 bg-light-grayish-cyan flex items-center px-3 gap-2 rounded-l-md text-desaturated-dark-cyan font-bold">{{ item }}</p>
-      <div class="bg-desaturated-dark-cyan h-8 w-8 flex items-center justify-center rounded-r-md cursor-pointer" @click="removeFilter(item)">
+      <div class="bg-desaturated-dark-cyan hover:bg-very-dark-grayish-cyan h-8 w-8 flex items-center justify-center rounded-r-md cursor-pointer" @click="removeFilter(item)">
         <img src="/src/assets/images/icon-remove.svg" />
       </div>
     </div>
@@ -23,7 +23,7 @@
     >
       <div class="flex flex-col md:flex-row gap-4">
         <img :src="getLogoPath(job.logo)" :alt="job.company" class="h-12 md:h-auto object-contain mr-auto md:mr-0 -mt-8" />
-        <div class="flex flex-col justify-between gap-4 md:gap-0">
+        <div class="flex flex-col justify-between gap-4 md:gap-2">
           <div class="flex gap-2 items-center">
             <p class="text-desaturated-dark-cyan font-bold">{{ job.company }}</p>
             <p v-if="job.new" class="relative before:block before:absolute before:-inset-1 before:bg-desaturated-dark-cyan before:rounded-full h-[18px]">
@@ -33,7 +33,7 @@
               <span class="relative text-white uppercase font-medium px-3">featured</span>
             </p>
           </div>
-          <h4 class="text-very-dark-grayish-cyan font-bold text-xl">{{ job.position }}</h4>
+          <h4 class="text-very-dark-grayish-cyan hover:text-desaturated-dark-cyan font-bold text-xl">{{ job.position }}</h4>
           <div class="flex items-center gap-2">
             <p class="flex items-center text-dark-grayish-cyan gap-2 after:size-1 after:bg-dark-grayish-cyan after:rounded-full">{{ job.postedAt }}</p>
             <p class="flex items-center text-dark-grayish-cyan gap-2 after:size-1 after:bg-dark-grayish-cyan after:rounded-full">{{ job.contract }}</p>
@@ -45,7 +45,7 @@
         <p
           v-for="item in [...job.tools, ...job.languages]"
           :key="item"
-          class="flex items-center text-desaturated-dark-cyan font-bold bg-light-grayish-cyan h-8 px-3 rounded-sm cursor-pointer"
+          class="flex items-center text-desaturated-dark-cyan hover:text-white font-bold bg-light-grayish-cyan hover:bg-desaturated-dark-cyan h-8 px-3 rounded-md cursor-pointer"
           @click="addFilter(item)"
         >
           {{ item }}
